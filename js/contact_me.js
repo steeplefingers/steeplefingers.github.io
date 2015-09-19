3e9c15dae746d672c5c529cb2537e4d63c01892b
steeplefingers <code_monkey@slashmail.org>
2015-09-18 22:07:09 -0500

    can't submit?

--- js/contact_me.js
+++ js/contact_me.js
@@ -6,7 +6,7 @@ $(function() {
             // additional error messages or events
         },
         submitSuccess: function($form, event) {
-            event.preventDefault(); // prevent default submit behaviour
+            // event.preventDefault(); // prevent default submit behaviour
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
