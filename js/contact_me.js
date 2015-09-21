d2a893c481e82a37ce02fc5f96bdbf4ac31327bb
steeplefingers <code_monkey@slashmail.org>
2015-09-21 06:24:52 -0500

    lots of changes
    redid logos with transparent background, added logos for services, fixed
    the error message in the contact form. many many more changes coming.

--- js/contact_me.js
+++ js/contact_me.js
@@ -45,7 +45,8 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    // $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please send an email to contact@archdevops.com. Apologies for the invonvenience :(");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
